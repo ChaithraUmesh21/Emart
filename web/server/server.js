52e16bffe477258e46b8106f02c702cb11afabce
ChaithraUmesh21 <138147671+ChaithraUmesh21@users.noreply.github.com>
2024-07-07 13:17:56 +0100

    bugfix

--- web/server/server.js
+++ web/server/server.js
@@ -27,4 +27,12 @@ app.use('/api/categories', require('./routes/categories'));
 
 const PORT = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client', 'build')));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+  
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
